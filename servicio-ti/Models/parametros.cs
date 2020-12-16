d1e8a9ba947786d36c860ce6cd0e44eaa381d384
skraazh <lukas.munozm@utem.cl>
2020-12-16 16:18:42 -0300

    añadido funcion leer_C y leer_P

--- servicio-ti/Models/parametros.cs
+++ servicio-ti/Models/parametros.cs
@@ -14,12 +14,11 @@ namespace servicio_ti
         List<string> punto_ventas;
         int count_string;
 
-        public string leer(String ubicacion_archivo)
+        public List<string> leer()
         {
             centro_ventas = new List<string>();
             punto_ventas = new List<string>();
             int count_string = 0;
-
             StreamReader leer = new StreamReader(@"C:\texto\mi_texto.txt");
 
             while (!leer.EndOfStream)
@@ -36,23 +35,61 @@ namespace servicio_ti
                 if (dif == "P")
                     punto_ventas.Add(linea.Substring(2, (count_string - 2)));
             }
-
             Console.WriteLine("El contenido de punto ventas es:");
             foreach (string punto in punto_ventas)
             {
                 Console.WriteLine(punto);
             }
-
             Console.WriteLine("El contenido de centro ventas es:");
             foreach (string centro in centro_ventas)
             {
                 Console.WriteLine(centro);
             }
-
             Console.ReadLine();
+            return centro_ventas;
 
-            return "a";
+        }
 
+        public List<string> leer_C()
+        {
+            centro_ventas = new List<string>();
+            int count_string = 0;
+            StreamReader leer_C = new StreamReader(@"C:\texto\mi_texto.txt");
+
+            while (!leer_C.EndOfStream)
+            {
+                String linea = leer_C.ReadLine();
+                for (int i = 0; i < linea.Length; i++)
+                {
+                    count_string++;
+                }
+                string dif;
+                dif = linea.Substring(0, 1);
+                if (dif == "C")
+                    centro_ventas.Add(linea.Substring(2, (count_string - 2)));
+            }
+            return centro_ventas;
+        }
+
+        public List<string> leer_P()
+        {
+            punto_ventas = new List<string>();
+            int count_string = 0;
+            StreamReader leer_P = new StreamReader(@"C:\texto\mi_texto.txt");
+
+            while (!leer_P.EndOfStream)
+            {
+                String linea = leer_P.ReadLine();
+                for (int i = 0; i < linea.Length; i++)
+                {
+                    count_string++;
+                }
+                string dif;
+                dif = linea.Substring(0, 1);
+                if (dif == "P")
+                    punto_ventas.Add(linea.Substring(2, (count_string - 2)));
+            }
+            return punto_ventas;
         }
 
     }
